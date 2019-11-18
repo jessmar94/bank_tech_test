37170c8174ff8795a138fbed6bbe0f1602e63a98
Jessica Marais <jessica_marais@hotmail.com>
2019-11-18 14:16:52 +0000

    Statement prints with 0 balance when account starts with 0 balance and no deposit/withdrawal made:

---
+++ lib/bank_account.rb
@@ -0,0 +1,11 @@
+class Account
+
+  def initialize
+    @balance = 0
+  end
+
+  def print_statement
+    return "date || credit || debit || balance\n || || || 0.00"
+  end
+
+end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,4 +1,10 @@
+require 'bank_account' 
+
 describe Account do
 
-  describe ''
-end 
+  describe '#print_statement' do
+    it 'prints a statement with a balance of 0 when no deposits or withdrawals made' do
+      expect(subject.print_statement).to eq "date || credit || debit || balance\n || || || 0.00"
+    end
+  end
+end
