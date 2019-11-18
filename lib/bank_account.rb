2029882054f5cf893e61763f443eb73a52cb778f
Jessica Marais <jessica_marais@hotmail.com>
2019-11-18 15:30:21 +0000

    Refactor with private methods to format amount and balance

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,17 +5,26 @@ class Account
   end
 
   def get_balance
-    return ("%.2f" % @balance)
+    return format_balance(@balance) 
   end
 
   def deposit(amount)
     @balance += amount
-    credit = ("%.2f" % amount)
-    return @deposit_made = "#{Time.now.strftime("%d/%m/%Y")} || #{credit} || || #{get_balance}"
+    return @deposit_made = "#{Time.now.strftime("%d/%m/%Y")} || #{format_amount(amount)} || || #{get_balance}"
   end
 
   def print_statement
     return "date || credit || debit || balance\n#{@deposit_made}"
+  end
+
+  private
+
+  def format_amount(amount)
+    ("%.2f" % amount)
+  end
+
+  def format_balance(balance)
+    ("%.2f" % @balance)
   end
 
 end
