df4ad4b1f5195592e5cb84fb0e60fe99dc294c98
Jessica Marais <jessica_marais@hotmail.com>
2019-11-18 15:27:46 +0000

    User can print statement with date, credit and balance

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,15 +5,17 @@ class Account
   end
 
   def get_balance
-    return @balance
+    return ("%.2f" % @balance)
   end
 
   def deposit(amount)
-    @balance += amount 
+    @balance += amount
+    credit = ("%.2f" % amount)
+    return @deposit_made = "#{Time.now.strftime("%d/%m/%Y")} || #{credit} || || #{get_balance}"
   end
 
   def print_statement
-    return "date || credit || debit || balance\n || || || #{("%.2f" % @balance)}"
+    return "date || credit || debit || balance\n#{@deposit_made}"
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,21 +3,22 @@ require 'bank_account'
 describe Account do
 
   describe '#print_statement' do
-    it 'prints a statement with a balance of 0 when no deposits or withdrawals made' do
-      expect(subject.print_statement).to eq "date || credit || debit || balance\n || || || 0.00"
+    it 'prints date: 10/01/2012, credit: 10, debit, balance:10 when deposit of 10 made' do
+      subject.deposit(10)
+      expect(subject.print_statement).to eq "date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y")} || 10.00 || || 10.00"
     end
   end
 
   describe '#get_balance' do
     it 'returns 0 when no money in the account' do
-      expect(subject.get_balance).to eq 0.00
+      expect(subject.get_balance).to eq "0.00"
     end
   end
 
   describe '#deposit' do
     it 'updates balance to 10 when deposit 10' do
       subject.deposit(10)
-      expect(subject.get_balance).to eq 10.00
+      expect(subject.get_balance).to eq "10.00"
     end
   end
 end
