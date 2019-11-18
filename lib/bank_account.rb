86ea76c3a5eb1ede5e1b87a878608d4396648f74
Jessica Marais <jessica_marais@hotmail.com>
2019-11-18 14:18:57 +0000

    User can check balance of account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,15 @@
 class Account
 
   def initialize
-    @balance = 0
+    @balance = 0.00
   end
 
   def print_statement
     return "date || credit || debit || balance\n || || || 0.00"
+  end
+
+  def get_balance
+    return @balance
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,10 +1,16 @@
-require 'bank_account' 
+require 'bank_account'
 
 describe Account do
 
   describe '#print_statement' do
     it 'prints a statement with a balance of 0 when no deposits or withdrawals made' do
       expect(subject.print_statement).to eq "date || credit || debit || balance\n || || || 0.00"
+    end
+  end
+
+  describe '#get_balance' do
+    it 'returns 0 when no money in the account' do
+      expect(subject.get_balance).to eq 0.00
     end
   end
 end
