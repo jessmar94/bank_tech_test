a9b1eb5cd0e5d2e2cb7a95a45d0a498719530ee4
Jessica Marais <jessica_marais@hotmail.com>
2019-11-19 14:16:01 +0000

    Rubocop fix

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -5,8 +5,9 @@ require 'transactions'
 describe Transactions do
   describe '#transaction_event' do
     it 'pushes deposit of 10 into transactions array' do
-      transactions = Transactions.new(['10-01-2012 || 10.00 || || 10.00'])
-      expect(transactions.transactions).to eq ['10-01-2012 || 10.00 || || 10.00']
+      transaction = ['10-01-2012 || 10.00 || || 10.00']
+      transactions = Transactions.new(transaction)
+      expect(transactions.transactions).to eq transaction
     end
   end
 end
