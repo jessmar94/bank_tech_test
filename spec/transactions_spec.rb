69ff6189b02e372ad7e5cde3f3920d582ebc7e19
Jessica Marais <jessica_marais@hotmail.com>
2019-11-27 20:58:41 +0000

    Update transaction_event test

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -5,9 +5,9 @@ require 'transactions'
 describe Transactions do
   describe '#transaction_event' do
     it 'pushes deposit of 10 into transactions array' do
-      transaction = ['10-01-2012 || 10.00 || || 10.00']
-      transactions = Transactions.new(transaction)
-      expect(transactions.transactions).to eq transaction
+      transactions = Transactions.new([])
+      transactions.transaction_event(deposit: 10)
+      expect(transactions.transactions).to eq [10]
     end
   end
 end
